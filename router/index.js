693cee066153cab9ea972ce298fd26480b5edb9f
sefati arora <sefatiarora@cqlsys.co.uk>
2025-11-03 11:02:10 +0530

    commit

--- router/index.js
+++ router/index.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Express" });
 });
 // router.get("/user", async (req, res) => {
-//   let jsonData = require("../config/productSwagger.json");
+//   let jsonData = require("../config/userSwagger.json");
 //   delete jsonData.host;
 //   jsonData.host = await commonHelper.getHost(req, res); // Dynamically set the host
 //   console.log("jsonData.host:  ", jsonData.host);
